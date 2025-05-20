3864a788cbe0d0724deddc4eba29eb726c2530c2
rohit <rohit.karnik@outlook.com>
2025-05-20 18:49:18 -0400

    wrote the code for merge sort and helper methods and created array to store animations

--- my-app/src/SortingVisualizer/SortingAlgorithms.js
+++ my-app/src/SortingVisualizer/SortingAlgorithms.js
@@ -1,21 +1,54 @@
-export const mergeSort = (arr, animations = []) => {
-    if (arr.length <= 1) return arr;
+export function mergeSort (arr, animations) {
+    if (arr.length <= 1) return;
     
-    const middleIndex = Math.floor(arr.length / 2)
-    const left = mergeSort(arr.slice(0,middleIndex))
-    const right = mergeSort(arr.slice(middleIndex))
+    const auxArray = arr.slice();
+    mergeSortHelper(arr, 0, arr.length-1, auxArray, animations);
+}
 
-    return merge(left,right)
-};
+function mergeSortHelper(mainArray, startIndex, endIndex, auxArray, animations) {
+    if (startIndex === endIndex) return;
+    const middle = Math.floor((startIndex + endIndex) / 2);
+    mergeSortHelper(auxArray, startIndex, middle, mainArray, animations);
+    mergeSortHelper(auxArray, middle+1, endIndex, mainArray, animations);
+    merge(mainArray, startIndex, middle, endIndex, auxArray, animations);
 
-const merge = (left, right, animations) => {
-    if (left.length === 0) return right;
-    if (right.length === 0) return left;
+}
 
-    if (left[0] < right[0]) {
-        return [left[0], ...mergeSort(left.slice(1), right)]
+export function getSortAnimations(array, sortAlgorithm) {
+    const animations = [];
+    if (array.length <= 1) return animations;
+    const arrayCopy = array.slice();
+    sortAlgorithm(arrayCopy, animations);
+    return animations;
+}
+
+
+function merge (mainArray, startIndex, middleIndex, endIndex, auxArray, animations) {
+    let k = startIndex;
+    let i = startIndex;
+    let j = middleIndex + 1
+
+    while (i <= middleIndex && j <= endIndex) {
+        animations.push(['compare', i, j]);
         
-    } else {
-        return [right[0], ...mergeSort(left, right.slice(1))]
+        if (auxArray[i] <= auxArray[j]) {
+            animations.push(['overwrite', k, null, auxArray[i]]);
+            mainArray[k++] = auxArray[i++];
+        } else {
+            animations.push(['overwrite', k , null, auxArray[j]]);
+            mainArray[k++] = auxArray[j++];
+        }
     }
-};
+
+    while (i <= middleIndex) {
+        animations.push(['overwrite', k, null, auxArray[i]]);
+        mainArray[k++] = auxArray[i++];
+    }
+
+    while (j <= endIndex) {
+        animations.push(['overwrite', k, null, auxArray[j]]);
+        mainArray[k++] = auxArray[j++];
+    }
+
+}
+
