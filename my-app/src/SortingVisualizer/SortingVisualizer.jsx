8f0de9015b33194cf46907bcd822f0a5073e790a
rohit <rohit.karnik@outlook.com>
2025-05-21 19:33:25 -0400

    added a slider which updates array size

--- my-app/src/SortingVisualizer/SortingVisualizer.jsx
+++ my-app/src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,99 +1,124 @@
 import React from "react";
-import './SortingVisualizer.css'
-import {mergeSort, bubbleSort, quickSort, heapSort, getSortAnimations} from './SortingAlgorithms.js';
+import "./SortingVisualizer.css";
+import {
+  mergeSort,
+  bubbleSort,
+  quickSort,
+  heapSort,
+  getSortAnimations,
+} from "./SortingAlgorithms.js";
 
 export default class SortingVisualizer extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            arr: []
-        };
+    this.state = {
+      arr: [],
+    };
+  }
+
+  componentDidMount() {
+    this.newArray();
+  }
+
+  newArray(length) {
+    const arr = [];
+    for (let i = 0; i < length; i++) {
+      arr.push(randomInt(8, 800));
     }
+    this.setState({ arr });
+  }
 
-    componentDidMount() {
-        this.newArray();
-    }
+  mergeSort() {
+    const animations = getSortAnimations(this.state.arr, mergeSort);
+    animate(animations);
+  }
 
-    newArray() {
-        const arr = [];
-        for(let i = 0; i < 100; i++) {
-            arr.push(randomInt(8, 800));
-        }
-        this.setState({arr});
-    }
+  bubbleSort() {}
 
-    mergeSort() {
-        const animations = getSortAnimations(this.state.arr, mergeSort);
-        animate(animations)
+  quickSort() {}
 
-    }
+  heapSort() {}
 
-    bubbleSort() {
+  render() {
+    const { arr } = this.state;
 
-    }
-
-    quickSort() {
-
-    }
-
-    heapSort() {
-
-    }
-
-    render() {
-        const {arr} = this.state;
-
-        return (
-        <div className="array-container">
-            {arr.map((val, index) => (
-            <div
-                className="array-bar" 
-                key={index}
-                style = {{height: `${val}px`}}
-                >
-            </div>    
-            ))}
-            <button onClick={(() => this.newArray())}>Create New Array</button>
-            <button onClick={(() => this.mergeSort())}> Merge Sort</button>
-            <button onClick={(() => this.heapSort())}> Heap Sort</button>
-            <button onClick={(() => this.bubbleSort())}> Bubble Sort</button>
-            <button onClick={(() => this.quickSort())}> Quick Sort</button>
-
+    return (
+      <div>
+        <div className="header">
+          <h1>Sorting Algorithm Visualizer</h1>
+          <div className="buttons">
+            <button
+              className="button"
+              onClick={() => this.newArray(arr.length)}
+            >
+              Create New Array
+            </button>
+            <button className="button" onClick={() => this.mergeSort()}>
+              Merge Sort
+            </button>
+            <button className="button" onClick={() => this.heapSort()}>
+              Heap Sort
+            </button>
+            <button className="button" onClick={() => this.bubbleSort()}>
+              Bubble Sort
+            </button>
+            <button className="button" onClick={() => this.quickSort()}>
+              Quick Sort
+            </button>
+          </div>
+          <div className="slider-container">
+            <input
+              type="range"
+              min="10"
+              max="100"
+              value={arr.length}
+              class="slider"
+              id="myRange"
+              onChange={(e) => this.newArray(parseInt(e.target.value))}
+            ></input>
+          </div>
         </div>
-                
-        )
-    }
-
-    
+        <div className="array-container">
+          {arr.map((val, index) => (
+            <div
+              className="array-bar"
+              key={index}
+              style={{ height: `${val}px` }}
+            ></div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 function randomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function animate(animations) {
-    const arrayBars = document.getElementsByClassName("array-bar")
-    const ANIMATION_SPEED = 10;
+  const arrayBars = document.getElementsByClassName("array-bar");
+  const ANIMATION_SPEED = 10;
 
-    for (let i = 0; i < animations.length; i++) {
-        const animation = animations[i];
-        const [action, index1, index2, newHeight] = animation;
+  for (let i = 0; i < animations.length; i++) {
+    const animation = animations[i];
+    const [action, index1, index2, newHeight] = animation;
 
-        if (action === "compare") {
-            setTimeout(() => {
-                arrayBars[index1].style.backgroundColor = "red";
-                arrayBars[index2].style.backgroundColor = "red";
-            }, i * ANIMATION_SPEED)
+    if (action === "compare") {
+      setTimeout(() => {
+        arrayBars[index1].style.backgroundColor = "red";
+        arrayBars[index2].style.backgroundColor = "red";
+      }, i * ANIMATION_SPEED);
 
-            setTimeout(() => {
-                arrayBars[index1].style.backgroundColor = 'cyan'
-                arrayBars[index2].style.backgroundColor = 'cyan'
-            }, (i + 1) * ANIMATION_SPEED); 
-        } else if (action === 'overwrite') {
-            setTimeout(() => {
-                arrayBars[index1].style.height = `${newHeight}px`;
-            }, i * ANIMATION_SPEED);
-        }
+      setTimeout(() => {
+        arrayBars[index1].style.backgroundColor = "cyan";
+        arrayBars[index2].style.backgroundColor = "cyan";
+      }, (i + 1) * ANIMATION_SPEED);
+    } else if (action === "overwrite") {
+      setTimeout(() => {
+        arrayBars[index1].style.height = `${newHeight}px`;
+      }, i * ANIMATION_SPEED);
     }
-};
+  }
+}
