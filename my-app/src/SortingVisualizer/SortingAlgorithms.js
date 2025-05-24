eb2ce3071dac947a413aa07649bd9831c25a9297
rohit <rohit.karnik@outlook.com>
2025-05-23 20:39:45 -0400

    minor updates

--- my-app/src/SortingVisualizer/SortingAlgorithms.js
+++ my-app/src/SortingVisualizer/SortingAlgorithms.js
@@ -1,60 +1,69 @@
-export function mergeSort (arr, animations) {
-    if (arr.length <= 1) return;
-    
-    const auxArray = arr.slice();
-    mergeSortHelper(arr, 0, arr.length-1, auxArray, animations);
+export function mergeSort(arr, animations) {
+  if (arr.length <= 1) return;
+
+  const auxArray = arr.slice();
+  mergeSortHelper(arr, 0, arr.length - 1, auxArray, animations);
 }
 
 export function bubbleSort(arr, animations) {
-    if (arr.length <= 1) return;
-    const auxArray = arr.slice();
-    // bubbleSortHelper(arr, )
+  if (arr.length <= 1) return;
+  const auxArray = arr.slice();
+  // bubbleSortHelper(arr, )
 }
 
-function mergeSortHelper(mainArray, startIndex, endIndex, auxArray, animations) {
-    if (startIndex === endIndex) return;
-    const middle = Math.floor((startIndex + endIndex) / 2);
-    mergeSortHelper(auxArray, startIndex, middle, mainArray, animations);
-    mergeSortHelper(auxArray, middle+1, endIndex, mainArray, animations);
-    merge(mainArray, startIndex, middle, endIndex, auxArray, animations);
-
+function mergeSortHelper(
+  mainArray,
+  startIndex,
+  endIndex,
+  auxArray,
+  animations
+) {
+  if (startIndex === endIndex) return;
+  const middle = Math.floor((startIndex + endIndex) / 2);
+  mergeSortHelper(auxArray, startIndex, middle, mainArray, animations);
+  mergeSortHelper(auxArray, middle + 1, endIndex, mainArray, animations);
+  merge(mainArray, startIndex, middle, endIndex, auxArray, animations);
 }
 
 export function getSortAnimations(array, sortAlgorithm) {
-    const animations = [];
-    if (array.length <= 1) return animations;
-    const arrayCopy = array.slice();
-    sortAlgorithm(arrayCopy, animations);
-    return animations;
+  const animations = [];
+  if (array.length <= 1) return animations;
+  const arrayCopy = array.slice();
+  sortAlgorithm(arrayCopy, animations);
+  return animations;
 }
 
+function merge(
+  mainArray,
+  startIndex,
+  middleIndex,
+  endIndex,
+  auxArray,
+  animations
+) {
+  let k = startIndex;
+  let i = startIndex;
+  let j = middleIndex + 1;
 
-function merge (mainArray, startIndex, middleIndex, endIndex, auxArray, animations) {
-    let k = startIndex;
-    let i = startIndex;
-    let j = middleIndex + 1
+  while (i <= middleIndex && j <= endIndex) {
+    animations.push(["compare", i, j]);
 
-    while (i <= middleIndex && j <= endIndex) {
-        animations.push(['compare', i, j]);
-        
-        if (auxArray[i] <= auxArray[j]) {
-            animations.push(['overwrite', k, null, auxArray[i]]);
-            mainArray[k++] = auxArray[i++];
-        } else {
-            animations.push(['overwrite', k , null, auxArray[j]]);
-            mainArray[k++] = auxArray[j++];
-        }
+    if (auxArray[i] <= auxArray[j]) {
+      animations.push(["overwrite", k, null, auxArray[i]]);
+      mainArray[k++] = auxArray[i++];
+    } else {
+      animations.push(["overwrite", k, null, auxArray[j]]);
+      mainArray[k++] = auxArray[j++];
     }
+  }
 
-    while (i <= middleIndex) {
-        animations.push(['overwrite', k, null, auxArray[i]]);
-        mainArray[k++] = auxArray[i++];
-    }
+  while (i <= middleIndex) {
+    animations.push(["overwrite", k, null, auxArray[i]]);
+    mainArray[k++] = auxArray[i++];
+  }
 
-    while (j <= endIndex) {
-        animations.push(['overwrite', k, null, auxArray[j]]);
-        mainArray[k++] = auxArray[j++];
-    }
-
+  while (j <= endIndex) {
+    animations.push(["overwrite", k, null, auxArray[j]]);
+    mainArray[k++] = auxArray[j++];
+  }
 }
-
